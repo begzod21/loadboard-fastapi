085f840eafd44a5ab8e2a84c405dce8123a51b7f
rasulev.021 <rasulev.021@gmail.com>
2026-07-21 18:56:46 +0500

    check

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from app.core.database import engine, close_db, warmup
-from app.middleware import tenant_middleware, CORSMiddleware
-
-from fastapi.middleware.gzip import GZipMiddleware
+from app.middleware import tenant_middleware, GZipMiddleware, CORSMiddleware
 
 from app.api import load_router, vehicle_router
 
@@ -24,7 +22,7 @@ app = FastAPI(
 )
 
 app.middleware("http")(tenant_middleware)
-app.add_middleware(GZipMiddleware, minimum_size=1000, compresslevel=8)
+app.add_middleware(GZipMiddleware, minimum_size=1000)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
