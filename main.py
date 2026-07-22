bb778165fb4b6e1967c5afba04054f28f8c67c8c
rasulev.021 <rasulev.021@gmail.com>
2026-07-22 18:02:52 +0500

    change size gzip

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 )
 
 app.middleware("http")(tenant_middleware)
-app.add_middleware(GZipMiddleware, minimum_size=1000)
+app.add_middleware(GZipMiddleware, minimum_size=50000)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
