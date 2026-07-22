ae6f1846d434afa01320521934c3b2353738c14f
rasulev.021 <rasulev.021@gmail.com>
2026-07-22 13:40:21 +0500

    check

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -5,20 +5,22 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.database import AsyncSessionLocal
 from app.schemas.company import TenantCompanyOut
 
-from app.core.database import engine
-
 async def get_tenant(request: Request) -> TenantCompanyOut:
     if not hasattr(request.state, "tenant"):
         raise Exception("Tenant not set in request state. Ensure tenant_middleware is applied.")
     return request.state.tenant
 
 async def get_tenant_db(
-        tenant: TenantCompanyOut = Depends(get_tenant),
-) -> AsyncSession: # type: ignore
-    print("Before:", engine.sync_engine.pool.status())
+    tenant: TenantCompanyOut = Depends(get_tenant),
+):
     async with AsyncSessionLocal() as session:
-        async with session.begin():
-            await session.execute(text(f"SET search_path TO {tenant.schema_name}, public"))
-            yield session
+        await session.execute(
+            text(f'SET search_path TO "{tenant.schema_name}", public')
+        )
 
-    print("After:", engine.sync_engine.pool.status())
+        print("BEFORE")
+        try:
+            yield session
+        finally:
+            print("AFTER")
+            await session.rollback()
