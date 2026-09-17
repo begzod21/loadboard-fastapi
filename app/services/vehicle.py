ee7d18adf71806b96a1775f9c09d94556655233d
rasulev.021 <rasulev.021@gmail.com>
2026-09-17 12:22:41 +0500

    checking

--- app/services/vehicle.py
+++ app/services/vehicle.py
@@ -219,23 +219,7 @@ class VehicleListService:
                 ConfirmedLoad.is_deleted.is_(False),
             )
         )
-        driver_bid_exists = (
-            exists(
-                select(DriverBid.id).where(
-                    DriverBid.vehicle_id == Vehicle.id,
-                    DriverBid.load_id == load_id,
-                    DriverBid.vehicle_id.is_not(None),
-                    DriverBid.is_deleted.is_(False),
-                )
-            )
-            if load_id
-            else literal(False)
-        )
-        is_dbv = (
-            Vehicle.id.in_(driver_bid_vehicle_ids)
-            if driver_bid_vehicle_ids
-            else driver_bid_exists
-        )
+        is_dbv = Vehicle.id.in_(driver_bid_vehicle_ids) if driver_bid_vehicle_ids else literal(False)
 
         def base_filter():
             cond = and_(
